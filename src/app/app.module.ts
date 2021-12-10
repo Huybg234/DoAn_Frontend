d90e879207fd62260499b4eecedae7bd5a0ff430
kietdo <kietdots@gmail.com>
2021-12-10 15:12:37 +0700

    DoAn01

---
+++ src/app/app.module.ts
@@ -0,0 +1,37 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+import { AdminModule } from './admin/admin.module';
+import { RecruitmentComponent } from './admin/recruitment/recruitment.component';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NotFoundComponent } from './not-found/not-found.component';
+import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { FormsModule } from '@angular/forms';
+// import { AuthInterceptor } from './interceptor/auth.interceptor';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NotFoundComponent,
+    // RecruitmentComponent,
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule,
+    HttpClientModule,
+    BrowserAnimationsModule,
+    FormsModule
+  ],
+  providers: [
+    // {
+    //   // provide: HTTP_INTERCEPTORS,
+    //   // useClass: AuthInterceptor,
+    //   // multi: true,
+    // }
+  ],
+  bootstrap: [AppComponent]
+})
+export class AppModule {}
